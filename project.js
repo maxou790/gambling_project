d909ab57f97e4dce7cafd27f2135a242ae0466c2
maxou790 <maximservign@gmail.com>
2024-10-24 13:17:42 +0200

    Progressing else return

--- project.js
+++ project.js
@@ -1,12 +1,17 @@
 const prompt = require("prompt-sync")();
 
 const deposit =50 () => {
+while (true) {
   const depositAmount = prompt("Enter a deposit amount");
   const numberDepositAmount = parseFloat(depositAmount);
 
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+   if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
      console.log("Invalid deposit amount, try again.");
+  } else {
+    return numberDepositAmount;
   }
 }
+}
 
-deposit();
+const depositAmount= deposit();
+
