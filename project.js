8c8d232132c6e648821c336fc550130e2ff7083f
maxou790 <maximservign@gmail.com>
2024-10-24 12:13:29 +0200

    commit

--- project.js
+++ project.js
@@ -1,4 +1,12 @@
-const deposit () {
-  
+const prompt = require("prompt-sync")();
+
+const deposit =50 () => {
+  const depositAmount = prompt("Enter a deposit amount");
+  const numberDepositAmount = parseFloat(depositAmount);
+
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+     console.log("Invalid deposit amount, try again.");
+  }
 }
 
+deposit();
